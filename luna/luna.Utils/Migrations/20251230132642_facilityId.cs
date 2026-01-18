dcc31fc7e693612d6be7e232e5378fbb9e6a1594
Seojun Yun <me@lunalight.place>
2026-01-18 20:33:04 +0900

    migration change

--- luna/luna.Utils/Migrations/20251230132642_facilityId.cs
+++ luna/luna.Utils/Migrations/20251230132642_facilityId.cs
@@ -15,21 +15,21 @@ namespace luna.Migrations
             migrationBuilder.AlterDatabase()
                 .Annotation("MySql:CharSet", "utf8mb4");
 
-            // migrationBuilder.CreateTable(
-            //     name: "__efmigrationshistory",
-            //     columns: table => new
-            //     {
-            //         MigrationId = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false, collation: "utf8mb4_general_ci")
-            //             .Annotation("MySql:CharSet", "utf8mb4"),
-            //         ProductVersion = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false, collation: "utf8mb4_general_ci")
-            //             .Annotation("MySql:CharSet", "utf8mb4")
-            //     },
-            //     constraints: table =>
-            //     {
-            //         table.PrimaryKey("PRIMARY", x => x.MigrationId);
-            //     })
-            //     .Annotation("MySql:CharSet", "utf8mb4")
-            //     .Annotation("Relational:Collation", "utf8mb4_general_ci");
+            migrationBuilder.CreateTable(
+                name: "__efmigrationshistory",
+                columns: table => new
+                {
+                    MigrationId = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProductVersion = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.MigrationId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_general_ci");
 
             migrationBuilder.CreateTable(
                 name: "card",
